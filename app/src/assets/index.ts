4039b77bf44bb7b66445c52fb19aeec575dd8f73
junukim <junukim.dev@gmail.com>
2020-07-07 11:50:45 +0900

    [ASSETS] add new images

--- app/src/assets/index.ts
+++ app/src/assets/index.ts
@@ -1,4 +1,17 @@
 import BackgroundImg from "./backgroundImg.png";
 import LoadingImg from "./loading.gif";
+import ImageSave from "./imageSave.png";
+import accessTime from "./accessTime.png";
+import eventAvailable from "./eventAvailable.png";
+import facebook from "./facebook.png";
+import openInNew from "./openInNew.png";
 
-export { BackgroundImg, LoadingImg };
+export {
+  BackgroundImg,
+  LoadingImg,
+  ImageSave,
+  accessTime,
+  eventAvailable,
+  facebook,
+  openInNew,
+};
