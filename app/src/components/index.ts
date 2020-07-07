a0bdab32d1fdd554aa4aa6417664080378c7a51e
junukim <junukim.dev@gmail.com>
2020-07-07 11:54:09 +0900

    [MODIFY] add components

--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -1,4 +1,6 @@
 import Main from "./main";
 import Header from "./header";
+import Footer from "./footer";
+import Privacy from "./common/policy/Privacy";
 
-export { Main, Header };
+export { Main, Header, Footer, Privacy };
