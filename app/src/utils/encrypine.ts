f12d8b13f8b2f9782f1e42f4ad0024e0c50a6ad4
junukim <junukim.dev@gmail.com>
2020-08-13 10:08:48 +0900

    [MODIFY] util files clean up

---
+++ app/src/utils/encrypine.ts
@@ -0,0 +1,7 @@
+export const getUUID = () => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    var r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 3) | 8;
+    return v.toString(16);
+  });
+};
--- app/src/utils/input/index.ts
+++
@@ -1,5 +0,0 @@
-export const handleKeyPress = (key: string, event: any) => {
-  if (key === "Enter") {
-    event();
-  }
-};
